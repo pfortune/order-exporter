ef634515b95b9e959f6730c571d875e5c76adc70
pfortune <pfortune@gmail.com>
2019-10-05 16:31:24 +0100

    Added building and writing CSV order file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Prestashop = require("./utils/prestashop");
+const fs = require("fs");
 const ftp = require("./utils/ftp");
 
 require("dotenv").config();
@@ -12,6 +13,10 @@ presta.getOrders("payment_accepted", (error, result) => {
   if (error) {
     return console.log("Error", error);
   }
+
+  // Overwrites CSV each time and adds the headers
+  addCSVHeader();
+
   const orders = result.orders;
 
   for (let order in orders) {
@@ -20,34 +25,55 @@ presta.getOrders("payment_accepted", (error, result) => {
         return console.log("Error", error);
       }
 
-      const {
-        company,
-        firstname,
-        lastname,
-        id: orderId,
-        date_add: date,
-        id_customer: customerId,
-        address1,
-        address2,
-        city,
-        state,
-        postcode,
-        countryIso,
-        email,
-        phone,
-        itemslist: products
-      } = result.data;
-
-      console.log(
-        `
-         ${firstname} ${lastname} | ${orderId} | ${date} | ${customerId} | 
-         ${address1} | ${address2} | ${city} | ${state} | ${postcode} | ${countryIso} | ${email} | ${phone} | ${products}
-         --------
-         `
-      );
+      buildCSV(result.data);
     });
   }
 });
+
+const buildCSV = data => {
+  let order = "";
+
+  for (let i = 0; i < data.itemslist.length; i++) {
+    order += data.company + ", ";
+    order += data.firstname + ", ";
+    order += data.lastname + ", ";
+    order += data.id + ", ";
+    order += data.date_add + ", ";
+    order += data.itemslist[i].product_quantity + ", ";
+    order += data.id_customer + ", ";
+    order += data.address1 + ", ";
+    order += data.address2 + ", ";
+    order += " , ";
+    order += data.city + ", ";
+    order += data.state + ", ";
+    order += data.postcode + ", ";
+    order += " , ";
+    order += data.countryIso + ", ";
+    order += data.email + ", ";
+    order += data.itemslist[i].product_reference + ", ";
+    order += data.phone;
+
+    order += "\n";
+    appendToCSV(order);
+  }
+};
+
+// Adds the required headers to the top of the file
+const addCSVHeader = () => {
+  const header =
+    "Company,Firstname,Lastname,Order,Date,Quantity,Customer,Address1,Address2,Address3,City,State,Postcode,Other,Country,Email,Sku,Phone\n";
+  writeToCSV(header);
+};
+
+// Writes to the file, and overwrites existing data
+const writeToCSV = data => {
+  fs.writeFileSync("./data/test.csv", data);
+};
+
+// Appends a line to the file, without overwriting
+const appendToCSV = data => {
+  fs.appendFileSync("./data/test.csv", data);
+};
 
 /**
  * Test connecting to FTP server
