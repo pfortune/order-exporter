2b6446c82f622eebd07578055f2c51e4075dd6a3
pfortune <pfortune@gmail.com>
2019-10-13 19:57:16 +0100

    added formatPhoneNumber function

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const Prestashop = require('./utils/prestashop');
-const fs = require('fs');
-const ftp = require('./utils/ftp');
+const Prestashop = require("./utils/prestashop");
+const fs = require("fs");
+const ftp = require("./utils/ftp");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const presta = new Prestashop(process.env.URL, process.env.APIKEY);
 
@@ -12,9 +12,9 @@ const file = `./data/orders${Date.now()}.csv`;
 /**
  * Retrieve the ID of orders that are marked as payment accepted
  */
-presta.getOrders('payment_accepted', (error, result) => {
+presta.getOrders("payment_accepted", (error, result) => {
   if (error) {
-    return console.log('Error', error);
+    return console.log("Error", error);
   }
 
   // Create Header and then write to CSV file
@@ -26,7 +26,7 @@ presta.getOrders('payment_accepted', (error, result) => {
     // Retrieve the order details using the order ID
     presta.getOrderDetails(orders[order].id, (error, result) => {
       if (error) {
-        return console.log('Error', error);
+        return console.log("Error", error);
       }
 
       buildCSV(result.data);
@@ -39,7 +39,7 @@ presta.getOrders('payment_accepted', (error, result) => {
 
 // Creates each line of the CSV file from the order data
 const buildCSV = data => {
-  let order = '';
+  let order = "";
 
   for (let i = 0; i < data.itemslist.length; i++) {
     order += `"${data.company}", `;
@@ -51,25 +51,29 @@ const buildCSV = data => {
     order += `"${data.id_customer}", `;
     order += `"${data.address1}", `;
     order += `"${data.address2}", `;
-    order += ' , ';
+    order += " , ";
     order += `"${data.city}", `;
     order += `"${data.state}", `;
     order += `"${data.postcode}", `;
-    order += ' , ';
+    order += " , ";
     order += `"${data.countryIso}", `;
 
     order += `"${data.email}", `;
     order += `"${data.itemslist[i].product_reference}", `;
-    order += `"${data.phone.replace(/\D+/g, '')}"`;
+    order += `"${formatPhoneNumber(data.phone)}"`;
 
-    order += '\n';
+    order += "\n";
     writeToCSV(file, order);
   }
 };
 
+function formatPhoneNumber(number) {
+  return number.replace(/\D+/g, "");
+}
+
 // Required CSV headers
 const CSVHeader = () => {
-  return 'Company Name,First Name,Last Name,Order Number,Date of Order,Quantity,Account Number,Address Line 1,Address Line 2,Address Line 3,Town,County,Post Code,State,Country,Email Address,SKU,Phone number\n';
+  return "Company Name,First Name,Last Name,Order Number,Date of Order,Quantity,Account Number,Address Line 1,Address Line 2,Address Line 3,Town,County,Post Code,State,Country,Email Address,SKU,Phone number\n";
 };
 
 // Adds a line to a file, creates file if it doesn't exist
