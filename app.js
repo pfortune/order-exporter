04b6c2b472ae38428bc36b13b74722cb81ee0e7d
Peter Fortune <pfortune@gmail.com>
2019-10-16 02:43:56 +0100

    Added Order messages, and marking orders for Collection

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const Prestashop = require("./utils/prestashop");
-const fs = require("fs");
-const ftp = require("./utils/ftp");
+const Prestashop = require('./utils/prestashop');
+const fs = require('fs');
+const ftp = require('./utils/ftp');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const presta = new Prestashop(process.env.URL, process.env.APIKEY);
 
 // Create a unique file name
-const file = `./data/orders${Date.now()}.csv`;
+const file = `orders${Date.now()}.csv`;
+const local = `./data/${file}`;
 
 /**
  * Retrieve the ID of orders that are marked as payment accepted
  */
-presta.getOrders("payment_accepted", (error, result) => {
+presta.getOrders('payment_accepted', (error, result) => {
   if (error) {
-    return console.log("Error", error);
+    return console.log('Error', error);
   }
 
   // Create Header and then write to CSV file
-  writeToCSV(file, CSVHEADER);
+  writeToCSV(local, CSVHEADER);
 
   const orders = result.orders;
 
@@ -26,7 +27,7 @@ presta.getOrders("payment_accepted", (error, result) => {
     // Retrieve the order details using the order ID
     presta.getOrderDetails(orders[order].id, (error, result) => {
       if (error) {
-        return console.log("Error", error);
+        return console.log('Error', error);
       }
 
       buildCSV(result.data);
@@ -34,13 +35,14 @@ presta.getOrders("payment_accepted", (error, result) => {
   }
 
   // Upload orders to FTP server
-  uploadOrders(file);
+  uploadOrders(local);
 });
 
 // Creates each line of the CSV file from the order data
 const buildCSV = data => {
-  let order = "";
+  let order = '';
 
+  // if an order has more than one product, create a line for each product
   for (let i = 0; i < data.itemslist.length; i++) {
     order += `"${data.company}", `;
     order += `"${data.firstname}", `;
@@ -49,31 +51,46 @@ const buildCSV = data => {
     order += `"${data.date_add}", 1`;
     order += `"${data.itemslist[i].product_quantity}", `;
     order += `"${data.id_customer}", `;
-    order += `"${data.address1}", `;
-    order += `"${data.address2}", `;
-    order += " , ";
-    order += `"${data.city}", `;
-    order += `"${data.state}", `;
-    order += `"${data.postcode}", `;
-    order += " , ";
-    order += `"${data.countryIso}", `;
 
+    // if an order is for collection, replace all the address fields with 'Collection'
+    if (data.id_carrier === '193') {
+      order +=
+        'Collection,Collection,Collection,Collection,Collection,Collection,Collection,';
+    } else {
+      order += `"${data.address1}", `;
+      order += `"${data.address2}", `;
+      order += ' , ';
+      order += `"${data.city}", `;
+      order += `"${data.state}", `;
+      order += `"${data.postcode}", `;
+      order += ' , ';
+    }
+    order += `"${data.countryIso}", `;
     order += `"${data.email}", `;
     order += `"${data.itemslist[i].product_reference}", `;
-    order += `"${formatPhoneNumber(data.phone)}"`;
+    order += `"${formatPhoneNumber(data.phone)}", `;
 
-    order += "\n";
-    writeToCSV(file, order);
+    // check if there are any order messages, if there are none use an empty string
+    if (data.message === null) {
+      order += `""`;
+    } else {
+      order += `"${data.message}"`;
+    }
+    order += '\n';
+
+    // write the order to a CSV file locally
+    writeToCSV(local, order);
   }
 };
 
+// remove any character that's not a digit, including spaces
 function formatPhoneNumber(number) {
-  return number.replace(/\D+/g, "");
+  return number.replace(/\D+/g, '');
 }
 
 // Required CSV headers
 const CSVHEADER =
-  "Company Name,First Name,Last Name,Order Number,Date of Order,Quantity,Account Number,Address Line 1,Address Line 2,Address Line 3,Town,County,Post Code,State,Country,Email Address,SKU,Phone number\n";
+  'Company Name,First Name,Last Name,Order Number,Date of Order,Quantity,Account Number,Address Line 1,Address Line 2,Address Line 3,Town,County,Post Code,State,Country,Email Address,SKU,Phone number,Comments\n';
 
 // Adds a line to a file, creates file if it doesn't exist
 const writeToCSV = (file, data) => {
@@ -81,8 +98,7 @@ const writeToCSV = (file, data) => {
 };
 
 const uploadOrders = file => {
-  const remote = `/towms/orders${Date.now()}.csv`;
-
+  const remote = `/towms/${file}`;
   ftp.upload(file, remote);
-  ftp.connect();
+  // ftp.connect();
 };
