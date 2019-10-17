711e2ca24cb5832d474a18d2ad234b215ca428d7
Peter Fortune <pfortune@gmail.com>
2019-10-17 02:22:03 +0100

    removed a stray 1

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const Prestashop = require("./utils/prestashop");
-const fs = require("fs");
-const ftp = require("./utils/ftp");
+const Prestashop = require('./utils/prestashop');
+const fs = require('fs');
+const ftp = require('./utils/ftp');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const presta = new Prestashop(process.env.URL, process.env.APIKEY);
 
@@ -13,9 +13,9 @@ const local = `./data/${file}`;
 /**
  * Retrieve the ID of orders that are marked as payment accepted
  */
-presta.getOrders("payment_accepted", (error, result) => {
+presta.getOrders('payment_accepted', (error, result) => {
   if (error) {
-    return console.log("Error", error);
+    return console.log('Error', error);
   }
 
   // Create Header and then write to CSV file
@@ -27,7 +27,7 @@ presta.getOrders("payment_accepted", (error, result) => {
     // Retrieve the order details using the order ID
     presta.getOrderDetails(orders[order].id, (error, result) => {
       if (error) {
-        return console.log("Error", error);
+        return console.log('Error', error);
       }
 
       buildCSV(result.data);
@@ -40,7 +40,7 @@ presta.getOrders("payment_accepted", (error, result) => {
 
 // Creates each line of the CSV file from the order data
 const buildCSV = data => {
-  let order = "";
+  let order = '';
 
   // if an order has more than one product, create a line for each product
   for (let i = 0; i < data.itemslist.length; i++) {
@@ -48,29 +48,29 @@ const buildCSV = data => {
     order += `"${data.firstname}", `;
     order += `"${data.lastname}", `;
     order += `"${data.id}", `;
-    order += `"${data.date_add}", 1`;
+    order += `"${data.date_add}",`;
     order += `"${data.itemslist[i].product_quantity}", `;
     order += `"${data.id_customer}", `;
 
     // if an order is for collection, replace all the address fields with 'Collection'
-    if (data.id_carrier === "193") {
+    if (data.id_carrier === '193') {
       order +=
-        "Collection,Collection,Collection,Collection,Collection,Collection,Collection,";
+        'Collection,Collection,Collection,Collection,Collection,Collection,Collection,';
     } else {
       order += `"${data.address1}", `;
       order += `"${data.address2}", `;
-      order += " , ";
+      order += ' , ';
       order += `"${data.city}", `;
       order += `"${data.state}", `;
       order += `"${data.postcode}", `;
-      order += " , ";
+      order += ' , ';
     }
     order += `"${data.countryIso}", `;
     order += `"${data.email}", `;
     order += `"${data.itemslist[i].product_reference}", `;
     order += `"${formatPhoneNumber(data.phone)}", `;
     order += `"${formatMessage(data.message)}"`;
-    order += "\n";
+    order += '\n';
 
     // write the order to a CSV file locally
     writeToCSV(local, order);
@@ -79,7 +79,7 @@ const buildCSV = data => {
 
 // remove any character that's not a digit, including spaces
 function formatPhoneNumber(number) {
-  return number.replace(/\D+/g, "");
+  return number.replace(/\D+/g, '');
 }
 
 function formatMessage(message) {
@@ -87,12 +87,12 @@ function formatMessage(message) {
     return message.substring(0, 300);
   }
 
-  return "";
+  return '';
 }
 
 // Required CSV headers
 const CSVHEADER =
-  "Company Name,First Name,Last Name,Order Number,Date of Order,Quantity,Account Number,Address Line 1,Address Line 2,Address Line 3,Town,County,Post Code,State,Country,Email Address,SKU,Phone number,Comments\n";
+  'Company Name,First Name,Last Name,Order Number,Date of Order,Quantity,Account Number,Address Line 1,Address Line 2,Address Line 3,Town,County,Post Code,State,Country,Email Address,SKU,Phone number,Comments\n';
 
 // Adds a line to a file, creates file if it doesn't exist
 const writeToCSV = (file, data) => {
@@ -100,8 +100,8 @@ const writeToCSV = (file, data) => {
 };
 
 const uploadOrders = local => {
-  console.log("file:" + file);
+  console.log('file:' + file);
   const remote = `/towms/${file}`;
   ftp.upload(local, remote);
-  ftp.connect();
+  // ftp.connect();
 };
